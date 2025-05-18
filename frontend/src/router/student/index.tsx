f775eca0b34b9f95ef129cf4c7ca6038ec765101
almakeev <r.makeevv@gmail.com>
2025-05-19 02:50:32 +0300

    - UPDATE FE

---
+++ frontend/src/router/student/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { AdminHeader } from '../../components';
+import { UseLogOut } from '../../hooks';
+
+const StudentRoute = () => {
+    const logOut = UseLogOut();
+
+    return (
+        <div>
+            <AdminHeader logOut={logOut} />
+            <div style={{ marginTop: '80px' }}>
+                <h1>Страница ученика</h1>
+            </div>
+        </div>
+    );
+};
+
+export default StudentRoute;
