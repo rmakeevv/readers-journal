60654639e8ecbab25ba25bf68b8012d1b9652acb
almakeev <r.makeevv@gmail.com>
2025-06-19 20:29:24 +0300

    - UPDATE

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { IBook } from 'types';
+import { AssignedBook, IBook } from 'types';
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 
@@ -9,7 +9,7 @@ export const instance = axios.create({
 
 export const getOneBook = async (id: string) => {
     try {
-        const res = await instance.get<IBook[]>('book/' + id);
+        const res = await instance.get<IBook>('book/' + id);
         return res.data;
     } catch (e) {
         console.warn(e);
@@ -42,7 +42,69 @@ export const getChildrenWithBooks = async (userId: number) => {
 
 export const getAssignedBooksByChildId = async (userId: number) => {
     try {
-        return await instance.get(`users/${userId}/assigned-books`);
+        return await instance.get<AssignedBook[]>(
+            `users/${userId}/assigned-books`
+        );
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const assignBook = async (
+    child_id: number,
+    book_id: number,
+    parent_id: number
+) => {
+    try {
+        await instance.post('/users/assign', {
+            child_id,
+            book_id,
+            parent_id,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const startReadingBook = async (
+    child_id: number,
+    book_id: number,
+    parent_id: number
+) => {
+    try {
+        await instance.post('/users/start-reading', {
+            child_id,
+            book_id,
+            parent_id,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const completeReadingBook = async (
+    child_id: number,
+    book_id: number,
+    parent_id: number
+) => {
+    try {
+        await instance.post('/users/complete-reading', {
+            child_id,
+            book_id,
+            parent_id,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+export const fetchChildren = async (parent_id: number) => {
+    try {
+        const response = await instance.get(
+            '/users/' + parent_id + '/children'
+        );
+
+        return response.data;
     } catch (e) {
         console.log(e);
     }
